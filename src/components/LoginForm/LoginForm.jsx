d957cdcd235cff9f8f973a54ece2582adf23db6f
NataliiaPleskanka <154823178+NataliiaPleskanka@users.noreply.github.com>
2024-09-18 14:03:25 +0200

    k

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,24 @@
+const LoginForm = ({ onLogin }) => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    const form = evt.target;
+    const { login, password } = form.elements;
+
+    // Викликаємо пропс onLogin
+    onLogin({
+      login: login.value,
+      password: password.value,
+    });
+
+    form.reset();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="login" />
+      <input type="password" name="password" />
+      <button type="submit">Log in</button>
+    </form>
+  );
+};
+export default LoginForm;
